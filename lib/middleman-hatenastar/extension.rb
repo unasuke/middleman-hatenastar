6d8c046f396393473c92847945ea586a06008c65
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-05-21 23:30:25 +0900

    Add parameter

--- lib/middleman-hatenastar/extension.rb
+++ lib/middleman-hatenastar/extension.rb
@@ -4,9 +4,10 @@ require 'middleman-hatenastar/generator'
 module Middleman
   module Hatenastar
     class Extension < ::Middleman::Extension
-      option :uri,       'h3 a', 'Permarink of the entry.'
-      option :title,     'h3',   'Entry title.'
-      option :container, 'h3',   'Set hatenastar to after of the this tag.'
+      option :uri,        'h3 a', 'Permarink of the entry.'
+      option :title,      'h3',   'Entry title.'
+      option :container,  'h3',   'Set hatenastar to after of the this tag.'
+      option :entry_node, 'div.section', 'Unit of each entry,'
 
       def initialize(app, options_hash={}, &block)
         super
@@ -16,8 +17,8 @@ module Middleman
 
       expose_to_template :hatenastar
 
-      def hatenastar(uri: nil, title: nil, container: nil)
-        @generator.generate(uri: uri, title: title, container: container)
+      def hatenastar(uri: nil, title: nil, container: nil, entry_node: nil)
+        @generator.generate(uri: uri, title: title, container: container, entry_node: entry_node)
       end
     end
   end
--- lib/middleman-hatenastar/generator.rb
+++ lib/middleman-hatenastar/generator.rb
@@ -5,19 +5,21 @@ module Middleman
         @uri = option[:uri]
         @title = option[:title]
         @container = option[:container]
+        @entry_node = option[:entry_node]
       end
 
-      def generate(uri: nil, title: nil, container: nil)
+      def generate(uri: nil, title: nil, container: nil, entry_node: nil)
         _uri = uri || @uri
         _title = title || @title
         _container = container || @container
+        _entry_node = entry_node || @entry_node
 
         <<~TAG
           <script type="text/javascript" src="//s.hatena.ne.jp/js/HatenaStar.js"></script>
           <script type="text/javascript">
           Hatena.Star.SiteConfig = {
             entryNodes: {
-              'section': {
+              "#{_entry_node}": {
                 uri: "#{_uri}",
                 title: "#{_title}",
                 container: "#{_container}"
