b1dd9567d09d216b20d9bd22dfc1a338ff3ed25f
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-05-21 22:06:17 +0900

    Remove comments

--- lib/middleman-hatenastar/extension.rb
+++ lib/middleman-hatenastar/extension.rb
@@ -1,34 +1,5 @@
-# Require core library
 require 'middleman-core'
 
-# Extension namespace
-#class ::Middleman::Hatenastar::Extension < ::Middleman::Extension
-#  #option :my_option, 'default', 'An example option'
-#
-#  def initialize(app, options_hash={}, &block)
-#    # Call super to build options from the options_hash
-#    super
-#
-#    # Require libraries only when activated
-#    # require 'necessary/library'
-#
-#    # set up your extension
-#    # puts options.my_option
-#  end
-#
-#  #def after_configuration
-#  #  # Do something
-#  #end
-#
-#  # A Sitemap Manipulator
-#  # def manipulate_resource_list(resources)
-#  # end
-#
-#  helpers do
-#    def hatenastar
-#      puts "aaaaaa"
-#    end
-#  end
 module Middleman
   module Hatenastar
     class Extension < ::Middleman::Extension
