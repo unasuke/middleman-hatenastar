a711771ed6e9708617b2e8a0a6b8b188145294b0
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-05-22 01:01:38 +0900

    Add helper to embed hatenastar token tag

--- lib/middleman-hatenastar/extension.rb
+++ lib/middleman-hatenastar/extension.rb
@@ -4,6 +4,7 @@ require 'middleman-hatenastar/generator'
 module Middleman
   module Hatenastar
     class Extension < ::Middleman::Extension
+      option :token,      nil,    'Your hatenastar token.'
       option :uri,        'h3 a', 'Permarink of the entry.'
       option :title,      'h3',   'Entry title.'
       option :container,  'h3',   'Set hatenastar to after of the this tag.'
@@ -12,13 +13,21 @@ module Middleman
       def initialize(app, options_hash={}, &block)
         super
 
+        if options[:token] == nil
+          raise ArgumentError, 'Hatenastar token is required.'
+        end
+
         @generator = ::Middleman::Hatenastar::Generator.new(options)
       end
 
-      expose_to_template :hatenastar_tag
+      expose_to_template :hatenastar_tag, :hatenastar_token_tag
 
       def hatenastar_tag(uri: nil, title: nil, container: nil, entry_node: nil)
         @generator.generate(uri: uri, title: title, container: container, entry_node: entry_node)
+      end
+
+      def hatenastar_token_tag
+        @generator.token
       end
     end
   end
--- lib/middleman-hatenastar/generator.rb
+++ lib/middleman-hatenastar/generator.rb
@@ -2,10 +2,20 @@ module Middleman
   module Hatenastar
     class Generator
       def initialize(option)
+        @token = option[:token]
         @uri = option[:uri]
         @title = option[:title]
         @container = option[:container]
         @entry_node = option[:entry_node]
+      end
+
+      def token
+        <<~TOKEN
+          <script type="text/javascript" src="//s.hatena.ne.jp/js/HatenaStar.js"></script>
+          <script type="text/javascript">
+            Hatena.Star.Token = '#{@token}';
+          </script>
+        TOKEN
       end
 
       def generate(uri: nil, title: nil, container: nil, entry_node: nil)
